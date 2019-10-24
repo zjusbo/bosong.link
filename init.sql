88760fbc54f976e12bb70bc1c6592976390e562f
Bo <songbo.sunny@gmail.com>
2019-10-23 20:54:42 -0700

    add comment in sql file

--- init.sql
+++ init.sql
@@ -1,11 +1,12 @@
-CREATE DATABASE bosonglink;
+# Script to set up database
+# $ mysql < init.sql
+#
+CREATE DATABASE IF NOT EXISTS bosonglink;
 USE bosonglink;
-CREATE TABLE tablename ( id INT unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY, 
-	short_url VARCHAR(255) NOT NULL, 
+CREATE TABLE IF NOT EXISTS url ( id INT unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+	short_url VARCHAR(255) UNIQUE NOT NULL, 
 	original_url VARCHAR(65535) NOT NULL, 
 	owner_email VARCHAR(255), 
 	is_deleted BOOLEAN DEFAULT false, 
-	visit_count INT DEFAULT 0, 
-	creation_date DATE DEFAULT CURRENT_DATE, 
-	last_visited_date DEFAULT DATE CURRENT_DATE);
-INSERT INTO tablename ( id, name ) VALUES ( null, '')
+	creation_date DATE DEFAULT CURRENT_DATE);
+INSERT INTO url (short_url, original_url, owner_email) VALUES ('default', 'www.bo-song.com');
